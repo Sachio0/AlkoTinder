e014512949a1412c4c775add9f075b87ce800ff7
nowi.king <Nowi.King@gmail.com>
2021-06-10 18:52:20 +0200

    Create App

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+    }
+}
